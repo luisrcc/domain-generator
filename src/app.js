7cf8a0d92e6bc0dad7d5abf30709fcaae21242d0
luisrcc <luiscastillo1991@gmail.com>
2022-12-19 13:06:03 +0000

    domain-generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombre = parseInt(Math.random() * (pronoun.length + 1 - 1) + 1);
+  let adjetivo = parseInt(Math.random() * (adj.length + 1 - 1) + 1);
+  let nombre = parseInt(Math.random() * (noun.length + 1 - 1) + 1);
+
+  let concat =
+    pronoun[pronombre - 1] + adj[adjetivo - 1] + noun[nombre - 1] + ".com";
+
+  return concat;
 };
+
+document.getElementById("domain").innerText = onload();
